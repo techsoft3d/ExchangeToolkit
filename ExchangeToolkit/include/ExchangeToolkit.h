6a9a750611d17048204f85811aabe11e276f962a
Malcolm Tyrrell <matyrr@microsoft.com>
2021-08-17 08:21:58 +0100

    One more tweak

--- ExchangeToolkit/include/ExchangeToolkit.h
+++ ExchangeToolkit/include/ExchangeToolkit.h
@@ -2035,7 +2035,7 @@ namespace ts3d {
                         _texture.insert( _texture.end(), (vert % 2) ? next_t.begin() : prev_t.begin(), (vert % 2) ? next_t.end() : prev_t.end() );
                         _vertices.push_back( (vert % 2) ? next_v : prev_v );
                         
-                        _normals.push_back( vert % 2 ? prev_n : next_n );
+                        _normals.push_back( (vert % 2) ? prev_n : next_n );
                         _texture.insert( _texture.end(), (vert % 2) ? prev_t.begin() : next_t.begin(), (vert % 2) ? prev_t.end() : next_t.end() );
                         _vertices.push_back( (vert % 2) ? prev_v : next_v );
                     }
